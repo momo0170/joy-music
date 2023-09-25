67e1b5289061cf8ffdfb44b5071381eb350dc7af
이창한 <momo0170@naver.com>
2023-09-25 13:32:41 +0900

    Configure tailwind.config.js

--- .history/tailwind.config_20230924163404.js
+++ .history/tailwind.config_20230924163404.js
@@ -4,15 +4,15 @@ module.exports = {
   theme: {
     extend: {
       borderWidth: {
-        1.5: '1.5px',
+        1.5: '1.5px';,
       },
       boxShadow: {
         inner2: '0 0 0 1.5px #7c3aed inset',
         hoverInput: '0 0 0 1px #ABABAB inset',
       },
       borderColor: {
-        hoverInput: '#ABABAB',
-      },
+        hoverInput: '#ABABAB'
+      }
     },
   },
   plugins: [],
